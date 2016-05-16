dc1ae0cc18d97343cf8f417be5a3dd7b7951e8a2
Justin Unterreiner <justin.unterreiner@11main.com>
2016-05-16 14:33:31 -0700

    Fix for cardholder name not being returned

--- src/android/CardIOCordovaPlugin.java
+++ src/android/CardIOCordovaPlugin.java
@@ -117,6 +117,7 @@ public class CardIOCordovaPlugin extends CordovaPlugin {
             scanCard.put("expiryYear", card.expiryYear);
             scanCard.put("cvv", card.cvv);
             scanCard.put("postalCode", card.postalCode);
+            scanCard.put("cardholderName", card.cardholderName);
         } catch (JSONException e) {
             scanCard = null;
         }
